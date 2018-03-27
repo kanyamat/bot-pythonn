6ab33924e6ef9346ab2daa2cfaa6f2df0d8073e8
kanyamat <31055915+kanyamat@users.noreply.github.com>
2018-03-27 14:39:35 +0700

    a

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def reply(replyToken, textList):
     for text in textList:
         msgs.append({
             "type":"text",
-            "text":"hello"
+            "text":text
         })
     data = json.dumps({
         "replyToken":replyToken,
